ed99d98653a2452d7d77943df0ec72d36a380885
brianko <kobj11@gmail.com>
2021-12-19 16:46:27 -0500

    commit tree af43c24d2741698eeea7e9d959afdbc1a74f6e6c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const newCats = [...cats, "Broom"]
+   return newCats
+}
+function prependCat(name) {
+    const newCats = ["Arnold", ...cats]
+    return newCats
+}
+function removeLastCat(name) {
+    const newCats = cats.slice(0, cats.length - 1)
+    return newCats
+}
+function removeFirstCat(name) {
+    const newCats = cats.slice(1)
+    return newCats
+}
